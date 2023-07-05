ab16d5d282d0ffbebffce80f4485186d4054da9e
한승열 <hsy838148@gmail.com>
2023-07-05 16:34:59 +0900

    마지막작업 페이지버튼 및 버튼클릭시 페이지넘김 완료

--- listpage.js
+++ listpage.js
@@ -1,11 +1,6 @@
-// http://localhost:5500/contacts?pageno=값
 function getPageno() {
   const param = new URLSearchParams(location.search).get('pageno');
   const pageno = parseInt(param);
-
-  // pageno 가 없거나 숫자로 바꿀수 없는 값인 경우 parseInt의 결과는 NaN
-  // NaN를 비교하면 무조건 false(JS에서 NaN는 비교되는 값이 아니다)
-  // NaN와 비교되는
   if(isNaN(pageno))
     return 1;
   else if(pageno<1)
@@ -14,15 +9,64 @@ function getPageno() {
     return pageno;
 }
 
-// 기본 매개변수(default parameter)
 async function fetch(pageno, pagesize=10) {
   try {
     return await $.ajax(`http://sample.bmaster.kro.kr/contacts?pageno=${pageno}&pagesize=${pagesize}`);
-    // $.ajax()는 병렬 처리(비동기 처리)되는 코드 -> 언제 끝날 지 모른다
-    // 비동기코드를 리턴받는 result는 "미래의 값이 들어올 것이다"라는 값을 가진다
-    // (promise)
   } catch(err) {
     console.log(err);
     return null;
+  }
+}
+
+function getPagination({pageno, pagesize, totalcount, blockSize=5}) {
+  const countOfPage = Math.ceil(totalcount/pagesize);
+  const prev = Math.floor((pageno-1)/blockSize)*blockSize;
+  const start = prev+1;
+  let end = prev + blockSize;
+  let next = end + 1;
+  if(end>=countOfPage) {
+    end = countOfPage;
+    next = 0;
+  }
+  console.log({prev, start, end, next, pageno})
+  return {prev, start, end, next, pageno};
+}
+
+function printContacts(contacts, $parent) {
+  for(c of contacts) {
+    const html = `
+      <tr>
+        <td>${c.no}</td>
+        <td><a href='read.html?no=${c.no}'>${c.name}</a></td>
+        <td>${c.tel}</td>
+        <td>${c.address}</td>
+      </tr>
+    `;
+    $parent.append(html);
+  }
+}
+
+function printPagination({prev, start, end, next}, pageno, $parent) {
+  if(prev>0) {
+    const html =`
+      <li class='page-item'>
+        <a class='page-link' href='INCAlistpage.html?pageno=${prev}'>이전으로</a>
+      </li>`;
+    $parent.append(html);
+  }
+  for(let i=start; i<=end; i++) {
+    const className = pageno===i? 'page-item active' : 'page-item';
+    const html =`
+      <li class='${className}'>
+        <a class='page-link' href='INCAlistpage.html?pageno=${i}'>${i}</a>
+      </li>`;
+    $parent.append(html);  
+  }
+  if(next>0) {
+    const html =`
+      <li class='page-item'>
+        <a class='page-link' href='INCAlistpage.html?pageno=${next}'>다음으로</a>
+      </li>`;
+    $parent.append(html);
   }
 }
